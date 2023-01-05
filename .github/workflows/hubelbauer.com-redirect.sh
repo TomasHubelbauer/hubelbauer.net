195235bd791ab23caec0c1173a48b41f25aee29f
Tomáš Hübelbauer <tomas@hubelbauer.net>
2023-01-05 11:20:26 +0100

    Retry the network call with a delay in case the Location header is empty the first time around
    This happens sometimes but not too often. I will need to dig into why this is.

--- .github/workflows/hubelbauer.com-redirect.sh
+++ .github/workflows/hubelbauer.com-redirect.sh
@@ -3,6 +3,13 @@ PATTERN="Location: https://hubelbauer.net/"
 # Convert the Nginx response from CRLF to LF
 OUTPUT=`curl -I --no-progress-meter http://hubelbauer.com | grep "^Location" | tr -d "\r"`;
 
+# Retry the command if it returns an empty string the first time around which happens sometimes
+# TODO: Investigate why this happens by inspecting the response
+if [ -z "$OUTPUT" ]; then
+  sleep 1
+  OUTPUT=`curl -I --no-progress-meter http://hubelbauer.com | grep "^Location" | tr -d "\r"`;
+fi
+
 echo "Output:"
 echo "$OUTPUT" | tee output
 echo
