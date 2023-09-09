9612f5dbbf09e3adf58bd65e4d2c38548f130fd6
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-09-09 11:29:10 +0200

    Add a workflow to pull the updated certificate automatically and put it in the pattern file
    I will run this manually every 90 days as the certificate is rotated automatically by LE.

---
+++ .github/workflows/reset-https-ssl-certificates.sh
@@ -0,0 +1,60 @@
+# Send the `echo` to `openssl s_client`` otherwise it will wait for user input
+# Remove `SSL-Session` section items as they vary across runs and are irrelevant
+RAW=`echo | openssl s_client -showcerts -connect hubelbauer.net:443 | grep -v "^    "`
+
+OUTPUT=''
+
+# Convert BASE64 lines of certificate information into textual representation
+while IFS= read -r LINE; do
+  # Recognize start of BASE64 line set and mark the state variable tracking it
+  if [[ "$LINE" == "-----BEGIN CERTIFICATE-----" ]]; then
+    # Reset the temporary certificate file cert.crt - marker will be added below
+    echo "" > cert.crt
+
+    # Print the original line to the output to delineate the start of the cert
+    OUTPUT+="$LINE
+"
+
+    # Mark the state signifying reading the BASE64 line set without printing it
+    BASE64=1
+  fi
+
+  # Recognize end of BASE64 line set and dump the BASE64 text translation out
+  if [[ "$LINE" == "-----END CERTIFICATE-----" ]]; then
+    # Add the end of the certificate marker to the temporary certificate file
+    echo "$LINE" >> cert.crt
+
+    # Run the command to convert the lines to certificate textual representation
+    # Use `-noout` to make sure the certificate BASE64 is not in the output too
+    TEXT=`openssl x509 -in cert.crt -text -noout`
+
+    # Print the output of the `openssl -text` command to replace the BASE64 text
+    OUTPUT+="$TEXT
+"
+
+    # Delete the certificate temporary file to leave the FS tidy after our run
+    rm cert.crt
+
+    # Unmark the state tracking that we're in the BASE64 block of the text
+    unset BASE64
+  fi
+
+  # Redirect the BASE64 lines to the temporary certificate content file cert.crt
+  if [[ $BASE64 ]]; then
+    echo "$LINE" >> cert.crt
+
+    # Ignore the last statement in the loop to not print the BASE64 lines out
+    continue
+  fi
+
+  OUTPUT+="$LINE
+"
+done <<< "$RAW"
+
+# Drop the triailing newlines in the output to be comparable to the pattern
+OUTPUT=$(echo "$OUTPUT" | sed -Ez '$ s/\n+$//')
+
+echo "$OUTPUT" > .github/workflows/http-ssl-certificates.txt
+cat .github/workflows/http-ssl-certificates.txt
+pwd
+git status
