b0d5cdc0a3db86bdd31334939e3c982cff3576cf
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-12-27 21:43:50 +0100

    Print full URL in lint errors for clickability

--- .github/workflows/git-submodules/lint.ts
+++ .github/workflows/git-submodules/lint.ts
@@ -4,31 +4,33 @@ const modules = await parseModules();
 
 const errors: string[] = [];
 for (const module of modules) {
+  const id = module.url?.href ?? module.name;
+
   if (module.name !== module.path) {
-    errors.push(`Module "${module.name}" has a different name and path.`);
+    errors.push(`${id} has a different name and path.`);
   }
 
   if (!module.url) {
-    errors.push(`Module "${module.name}" is missing a URL.`);
+    errors.push(`${id} is missing a URL.`);
   }
   else if (module.url.hostname !== 'github.com') {
-    errors.push(`Module "${module.name}" has a non-GitHub URL.`);
+    errors.push(`${id} has a non-GitHub URL.`);
   }
 
   if (!module.title) {
-    errors.push(`Module "${module.name}" is missing a title.`);
+    errors.push(`${id} is missing a title.`);
   }
 
   if (!module.description) {
-    errors.push(`Module "${module.name}" is missing a description.`);
+    errors.push(`${id} is missing a description.`);
   }
 
   if (!module.homepage) {
-    errors.push(`Module "${module.name}" is missing a homepage.`);
+    errors.push(`${id} is missing a homepage.`);
   }
 
   if (module.topics.length === 0) {
-    errors.push(`Module "${module.name}" is missing topics.`);
+    errors.push(`${id} is missing topics.`);
   }
 }
 
